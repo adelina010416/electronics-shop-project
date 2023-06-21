741cd95b3c38226dc0eeb2e8dc7294e8fa457ed5
adelina010416 <adelinazima@mail.ru>
2023-06-21 15:19:06 +0400

    home_work_1 commit

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def laptop():
+    return Item('Laptop', 50000, 3)
+
+
+def test_calculate_total_price(laptop):
+    assert laptop.calculate_total_price() == 150000
+
+
+def test_apply_discount(laptop):
+    Item.pay_rate = 0.8
+    laptop.apply_discount()
+    assert laptop.price == 40000
